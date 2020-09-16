d96fc8749a5dcdc10788ab53f059424b88ea1f1d
Meen <peerapolmuadmu@gmail.com>
2020-09-16 13:37:35 +0700

    Create example.js

---
+++ js/example.js
@@ -0,0 +1,16 @@
+let PI = 3.14
+console.log("PI : ", PI)
+
+let test = "Hello"
+name = `${test} B`
+console.log(name)
+
+let arr = ['apple', 'banana', 'kiwi']
+console.log("arr : ", arr[1])
+
+let person = {
+    name: 'wow',
+    age: '20'
+}
+console.log(person)
+console.log(person.name, person.age)
