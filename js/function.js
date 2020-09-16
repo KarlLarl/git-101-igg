e474064d16eeb9f2934fa78d86bc9c3040110cdc
Meen <peerapolmuadmu@gmail.com>
2020-09-16 14:05:23 +0700

    Function [ +  * ]

---
+++ js/function.js
@@ -0,0 +1,18 @@
+function add(a, b) {
+    return a + b
+}
+console.log(add(2, 3))
+    // OR // 
+let adder = (a, b) => a + b
+console.log(adder(2, 3))
+
+//======================================= ( Function  +  เลข a b) ==================================
+function mul(a, b) {
+    return a * b
+}
+console.log(mul(10, 11))
+    // OR //
+let muler = (a, b) => a * b
+console.log(muler(10, 11))
+
+//======================================= ( Function  *  เลข a b) ==================================
